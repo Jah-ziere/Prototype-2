d3d9dddc36f5148b158d5711956a1aa2884b57aa
Jah-ziere <146846048+Jah-ziere@users.noreply.github.com>
2024-09-30 12:19:21 -0400

    create with code 2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
     float horizontalInput;
+    public float xRange =10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +17,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        //moves player on x axis using horizontal input
         transform.Translate(Vector3.right * 5 * Time.deltaTime * horizontalInput);
 
          //keep the player inbounds
-        if(transform.position.x < -10)
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-         if(transform.position.x > 10)
+         if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
     }
